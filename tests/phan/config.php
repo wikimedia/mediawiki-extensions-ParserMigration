d05fad0beace17776a830211d054f882942c15bf
Kunal Mehta <legoktm@member.fsf.org>
2018-06-08 13:43:55 -0700

    Don't manually suppress PhanDeprecatedProperty
    Already disabled by default in mediawiki-phan-config 0.3.0.
    
    Change-Id: I47da34f8162c7fd19cd45561ee2e9e66540ea3d5

--- tests/phan/config.php
+++ tests/phan/config.php
@@ -1,7 +1,3 @@
 <?php
 
-$cfg = require __DIR__ . '/../../vendor/mediawiki/mediawiki-phan-config/src/config.php';
-// MigrationEditPage::$mTitle false-positive
-$cfg['suppress_issue_types'][] = 'PhanDeprecatedProperty';
-
-return $cfg;
+return require __DIR__ . '/../../vendor/mediawiki/mediawiki-phan-config/src/config.php';
