0d59c859b835d68fa868a6c077f4f4e642297962
Umherirrender <umherirrender_de.wp@web.de>
2021-02-21 10:02:47 +0100

    build: Replace jsonlint with eslint
    Bug: T220036
    Change-Id: Ibbf0b4ff0e3d36bb27e5fec740da2440800e0d3b

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,19 @@
+/* eslint-env node, es6 */
 module.exports = function ( grunt ) {
 	var conf = grunt.file.readJSON( 'extension.json' );
 
-	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
 		banana: conf.MessagesDirs,
-		jsonlint: {
+		eslint: {
+			options: {
+				cache: true
+			},
 			all: [
-				'**/*.json',
+				'**/*.{js,json}',
 				'!node_modules/**',
 				'!vendor/**'
 			]
@@ -23,6 +27,6 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana', 'stylelint' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'banana', 'stylelint' ] );
 	grunt.registerTask( 'default', 'test' );
 };
