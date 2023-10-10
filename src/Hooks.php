aedec559a220f76e547b4b88f7af4047ca894887
sbailey <sbailey@wikimedia.org>
2023-10-10 15:35:16 -0700

    Adds developer tools page "use parsoid by default" checkbox
    Bug: T348257
    Change-Id: Ibb6586a5982169ef9b1b6987f5e0327f4e525cfb

--- src/Hooks.php
+++ src/Hooks.php
@@ -25,6 +25,13 @@ class Hooks implements
 			'label-message' => 'parsermigration-pref-label',
 			'help-message' => 'parsermigration-pref-help',
 			'section' => 'editing/developertools'
+			];
+
+		$defaultPreferences['parsermigration-parsoid-readviews'] = [
+			'type' => 'toggle',
+			'label-message' => 'parsermigration-parsoid-readviews-pref-label',
+			'help-message' => 'parsermigration-parsoid-readviews-pref-help',
+			'section' => 'editing/developertools'
 		];
 		return true;
 	}
@@ -38,15 +45,19 @@ class Hooks implements
 	public function onArticleParserOptions(
 		Article $article, ParserOptions $popts
 	) {
+		// T348257: Allow individual user to opt in to Parsoid read views as a ParserMigration option
+		$user = $article->getContext()->getUser();
+		$userOptionsManager = MediaWikiServices::getInstance()->getUserOptionsManager();
+		$userOptIn = $userOptionsManager->getOption( $user, 'parsermigration-parsoid-readviews' );
+
 		// T335157: Enable Parsoid Read Views for articles as an experimental
 		// feature; this is primarily used for internal testing at this time.
-		$request = $article->getContext()->getRequest();
-		$queryEnable = $request->getRawVal( 'useparsoid' );
-		if (
-			$queryEnable &&
-			// Allow disabling via config change to manage parser cache usage
-			\RequestContext::getMain()->getConfig()->get( 'ParserMigrationEnableQueryString' )
-		) {
+		$queryStringPresent = $article->getContext()->getRequest()->getRawVal( 'useparsoid' );
+
+		// Allow disabling via config change to manage parser cache usage
+		$queryStringEnabled = \RequestContext::getMain()->getConfig()->get( 'ParserMigrationEnableQueryString' );
+
+		if ( $userOptIn || ( $queryStringPresent && $queryStringEnabled ) ) {
 			$popts->setUseParsoid();
 		}
 		return true;
