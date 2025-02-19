42bac80debb0f3e759d9b51e48fb5da176726c66
Ed Sanders <esanders@wikimedia.org>
2025-02-19 15:56:05 +0000

    ESLint: Update to ES6 and autofix
    Change-Id: Iaf9c7f03b25696ed7031989ca6ac3934bf0b486b

--- resources/ext.parsermigration.notice.js
+++ resources/ext.parsermigration.notice.js
@@ -4,21 +4,21 @@
 ( function () {
 	// Bump this number any time you've made a significant change to the
 	// notice and want to redisplay it to the user.
-	var NOTICE_VERSION = mw.config.get( 'parsermigration-notice-version' );
+	const NOTICE_VERSION = mw.config.get( 'parsermigration-notice-version' );
 
 	// Don't show this message more than once/week.
-	var NOTICE_EXPIRY_SECONDS = mw.config.get( 'parsermigration-notice-days' ) *
+	const NOTICE_EXPIRY_SECONDS = mw.config.get( 'parsermigration-notice-days' ) *
 		60 * 60 * 24;
 
-	var STORAGE_KEY = 'mw-ext-parsermigration-notice-version';
+	const STORAGE_KEY = 'mw-ext-parsermigration-notice-version';
 
-	mw.hook( 'wikipage.content' ).add( function () {
+	mw.hook( 'wikipage.content' ).add( () => {
 		// Only display this notice if we're looking at parsoid content
 		if ( !mw.config.get( 'parsermigration-parsoid' ) ) {
 			return;
 		}
 		// Only display this notice once/week
-		var seen = mw.storage.getObject( STORAGE_KEY );
+		const seen = mw.storage.getObject( STORAGE_KEY );
 		if ( seen === false ) {
 			return; // storage is not available
 		}
@@ -27,19 +27,19 @@
 		}
 		// Build the message content from scratch in order to match/use Codex
 		// styles for a "notice" Message
-		var $title = $( '<p>' ).append(
+		const $title = $( '<p>' ).append(
 			$( '<strong>' ).html(
 				mw.message( 'parsermigration-notice-title' ).parse()
 			)
 		);
-		var $body = $( '<p>' ).html(
+		const $body = $( '<p>' ).html(
 			mw.message( 'parsermigration-notice-body' ).parse()
 		);
-		var msgBody = document.createElement( 'div' );
+		const msgBody = document.createElement( 'div' );
 		msgBody.appendChild( $title[ 0 ] );
 		msgBody.appendChild( $body[ 0 ] );
 
-		var $content = $( mw.util.messageBox( msgBody, 'notice', true ) );
+		const $content = $( mw.util.messageBox( msgBody, 'notice', true ) );
 		// mw.util.messageBox currently does not support close button so we have to append it.
 		$content.append(
 			$( '<span>' ).addClass( 'cdx-button__icon parsermigration-notice-icon-close' )
@@ -51,9 +51,9 @@
 				autoHide: false,
 				tag: 'parsermigration-notice'
 			}
-		).then( function ( notif ) {
+		).then( ( notif ) => {
 			// hook the close method
-			var oldClose = notif.close;
+			const oldClose = notif.close;
 			notif.close = function () {
 				// Once close, stay closed
 				mw.storage.setObject(
