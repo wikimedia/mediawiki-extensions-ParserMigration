d79f28f749371109b0914089ccd51f67c3608ca1
C. Scott Ananian <cscott@cscott.net>
2026-06-24 13:28:19 -0400

    Add $wgParserMigrationEnableParsoid as unified/fine-grained config
    This new configuration variable, when present, overrides any settings
    for the other variables and allows fine-grained configuration for
    opt-in/opt-out and percentage deployed on any combination of
    mobile/desktop and namespace.  Importantly, it lets us keep mobile
    deployments on enwiki at 100% even while gradually bumping up the
    percentage of main article space deployments.
    
    Change-Id: I2ace496df41c3af67df0f7acc5bd6aee85c2a2c3

--- tests/phpunit/OracleTest.php
+++ tests/phpunit/OracleTest.php
@@ -18,10 +18,11 @@ class OracleTest extends MediaWikiIntegrationTestCase {
 	 */
 	public function testIsParsoidDefaultFor(
 		string $title, bool $useParsoid, int $percent = 100,
-		array $excludePageProp = []
+		array $excludePageProp = [], ?array $enableParsoidConfig = null,
 	): void {
 		$this->overrideConfigValues( [
 			'ParserMigrationEnableQueryString' => true,
+			'ParserMigrationEnableParsoid' => $enableParsoidConfig,
 			'ParserMigrationEnableParsoidDiscussionTools' => true,
 			'ParserMigrationEnableParsoidArticlePages' => true,
 			'ParserMigrationEnableParsoidPercentage' => $percent,
@@ -62,5 +63,62 @@ class OracleTest extends MediaWikiIntegrationTestCase {
 		// 100% Parsoid percentage, but with (non)excluded page property
 		yield 'Excluded Page Property' => [ 'OracleTest1', false, 100, [ 'notoc' ] ];
 		yield 'Nonexcluded Page Property' => [ 'OracleTest2', true, 100, [ 'nocontentconvert' ] ];
+
+		// More tests, this time with the new-style unified configuration
+		$nc = "[new config] ";
+		$defaultArgs = [ 100, [] ];
+		yield "$nc Main Page" => [
+			'Main Page', true, ...$defaultArgs, [
+				'mobile' => false,
+				'default' => [ NS_MAIN => true ],
+			],
+		];
+		yield "$nc Page 1" => [
+			'Page 1', true, ...$defaultArgs, [
+				'mobile' => true,
+				'default' => [ NS_MAIN => 100 ],
+			],
+		];
+		yield "$nc Page 2" => [
+			'Page 2', false, ...$defaultArgs, [
+				'mobile' => true,
+				'default' => false,
+			],
+		];
+		// 50% Parsoid percentage, some of these result in true and some false.
+		$args = [ ...$defaultArgs, [
+			'default' => 50,
+		] ];
+		yield "$nc 50% Main Page" => [ 'Main Page', true, ...$args ];
+		yield "$nc 50% Page 1" => [ 'Page 1', false, ...$args ];
+		yield "$nc 50% Page 2" => [ 'Page 2', false, ...$args ];
+		yield "$nc 50% Page 3" => [ 'Page 3', true, ...$args ];
+		yield "$nc Talk:Main Page" => [ 'Talk:Main Page', true, ...$args ];
+		yield "$nc Talk:Page 1" => [ 'Talk:Page 1', false, ...$args ];
+		yield "$nc Talk:Page 2" => [ 'Talk:Page 2', true, ...$args ];
+		yield "$nc Talk:Page 3" => [ 'Talk:Page 3', true, ...$args ];
+		$nc = "[new config 2] ";
+		$args = [ ...$defaultArgs, [
+			'default' => [ NS_MAIN => 50, 'default' => 0 ],
+		] ];
+		yield "$nc 50% Main Page" => [ 'Main Page', true, ...$args ];
+		yield "$nc 50% Page 1" => [ 'Page 1', false, ...$args ];
+		yield "$nc 50% Page 2" => [ 'Page 2', false, ...$args ];
+		yield "$nc 50% Page 3" => [ 'Page 3', true, ...$args ];
+		// Talk pages
+		yield "$nc Talk:Main Page" => [ 'Talk:Main Page', false, ...$args ];
+		yield "$nc Talk:Page 1" => [ 'Talk:Page 1', false, ...$args ];
+		yield "$nc Talk:Page 2" => [ 'Talk:Page 2', false, ...$args ];
+		yield "$nc Talk:Page 3" => [ 'Talk:Page 3', false, ...$args ];
+		$nc = "[new config 3] ";
+		$args = [ ...$defaultArgs, [
+			'default' => [ NS_MAIN => 0, 'talk' => 50, 'default' => true ],
+		] ];
+		yield "$nc Main Page" => [ 'Main Page', false, ...$args ];
+		yield "$nc Talk:Main Page" => [ 'Talk:Main Page', true, ...$args ];
+		yield "$nc Talk:Page 1" => [ 'Talk:Page 1', false, ...$args ];
+		yield "$nc Talk:Page 2" => [ 'Talk:Page 2', true, ...$args ];
+		yield "$nc Talk:Page 3" => [ 'Talk:Page 3', true, ...$args ];
+		yield "$nc MediaWiki:Main Page" => [ 'MediaWiki:Main Page', true, ...$args ];
 	}
 }
